28d4014e1d6044bf66503454f377b0f9030bb1e1
TabMacedo1897 <tabata.macedo1897@gmail.com>
2023-04-02 22:57:04 -0300

    primeira versão bilbioteca eletrônica

---
+++ C76---ALUNOS-master/PRO_1-4_C75_ReferenciaDaProfessora1-main/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import BottomTabNavigator from "./components/BottomTabNavigator";
+
+export default class App extends Component {
+  render() {
+    //Meu teste de commit
+    //adicione a instrução de retorno para BottomTabNavigator
+    return <BottomTabNavigator />;
+  }
+}
